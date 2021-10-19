1ef3ee36a8c2f9aa1367f88b9255ea676a7aec93
denisdimarco <denisdimarcoleda@gmail.com>
2021-10-19 14:19:18 -0300

    Update myChart.js
    Se agrego la funcionalidad para pasar los datos al Grafico

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,15 +1,35 @@
+var chartDataStr = decodeHtml(chartData);
+var chartJsonArray = JSON.parse(chartDataStr);
+
+var arrayLength = chartJsonArray.length;
+
+var numericData = [];
+var labelData = [];
+
+for(var i=0; i < arrayLength; i++) {
+	
+	numericData[i] = chartJsonArray[i].value;
+	labelData[i] = chartJsonArray[i].label;
+} 
+
+
 new Chart(document.getElementById("myPieChart"), {
 	type: 'pie',
 	data: {
-		labels: ['Jannuary', 'February', 'March'],
+		labels: labelData,
 		datasets: [{
 			label: 'My first dataset',
 			backgroundColor: ["#001195", "#FFAA00", "#ADC6FF"],
 			borderColor: 'rgb(255,255,255)',
-			data: [2, 10, 5]
+			data: numericData
 		}]
 	},
-	options: {}
+	options: {
+		title: {
+			display: true,
+			text: 'Project Status'
+		}
+	}
 });
 
 function decodeHtml(html){
